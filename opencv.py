24669b8b1b3acdea6e2fa141a3fdc8e98097fdfe
Entity014 <radlove.012@gmail.com>
2022-12-21 23:22:19 +0700

    function v.1.3

--- opencv.py
+++ opencv.py
@@ -3,6 +3,28 @@ import mediapipe as mp
 import numpy as np
 import time
 
+def switch(argument):
+    if argument == 1:
+        videoCap()
+    elif argument == 2:
+        detectFinger()
+    elif argument == 3:
+        detectPos()
+    elif argument == 4:
+        detectSym()
+
+def Menu():
+    while True:
+        print("--- Menu ---")
+        print("1.Test Camera\n2.Detect Finger\n3.Detect Peosition of Finger\n4.Detect Symbol\n5.Exit")
+        ans = int(input("Select: "))
+        switch(ans)
+        if ans == 5:
+            print("Goodbye")
+            break
+        elif ans > 5:
+            print("Please enter in 1 - 5")
+
 def videoCap():
     cap = cv2.VideoCapture(0)
     while True:
@@ -140,6 +162,7 @@ def detectHand():
     hands = mpHands.Hands()
     mpDraw = mp.solutions.drawing_utils
     while cap.isOpened():
+        finger = []
         check, frame = cap.read()
         imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
@@ -149,58 +172,42 @@ def detectHand():
                     h, w, c = frame.shape
                     cx, cy = int(lm.x * w), int(lm.y * h)
                     arr[id] = [cx, cy]
-                if arr[0, 1] > arr[8, 1]:
-                    if arr[4, 0] > arr[17, 0]:
-                        if arr[0, 0] < arr[5, 0]:
-                            if arr[8, 1] < arr[5, 1]:
-                                print("นิ้วชี้ขวา")
-                            if arr[12, 1] < arr[9, 1]:
-                                print("นิ้วกลางขวา")
-                            if arr[16, 1] < arr[13, 1]:
-                                print("นิ้วนางขวา")
-                            if arr[20, 1] < arr[17, 1]:
-                                print("นิ้วก้อยขวา")
-                        if (arr[4, 0] > arr[8, 0]) and (arr[4, 1] < arr[2, 1]):
-                            print("นิ้วโป้งขวา")
-                    else:
-                        if arr[0, 0] > arr[5, 0]:
-                            if arr[8, 1] < arr[5, 1]:
-                                print("นิ้วชี้ซ้าย")
-                            if arr[12, 1] < arr[9, 1]:
-                                print("นิ้วกลางซ้าย")
-                            if arr[16, 1] < arr[13, 1]:
-                                print("นิ้วนางซ้าย")
-                            if arr[20, 1] < arr[17, 1]:
-                                print("นิ้วก้อยซ้าย")
-                        if (arr[4, 0] < arr[8, 0]) and (arr[4, 1] < arr[2, 1]):
-                            print("นิ้วโป้งซ้าย")
-                elif arr[0, 1] < arr[8, 1]:
-                    if arr[4, 0] < arr[20, 0]:
-                        if arr[0, 0] < arr[5, 0]:
-                            if arr[8, 1] > arr[5, 1]:
-                                print("นิ้วชี้ขวา")
-                            if arr[12, 1] > arr[9, 1]:
-                                print("นิ้วกลางขวา")
-                            if arr[16, 1] > arr[13, 1]:
-                                print("นิ้วนางขวา")
-                            if arr[20, 1] > arr[17, 1]:
-                                print("นิ้วก้อยขวา")
-                        if (arr[4, 0] > arr[8, 0]) and (arr[4, 1] > arr[2, 1]):
-                            print("นิ้วโป้งขวา")
-                    else:
-                        if arr[0, 0] > arr[5, 0]:
-                            if arr[8, 1] < arr[5, 1]:
-                                print("นิ้วชี้ซ้าย")
-                            if arr[12, 1] < arr[9, 1]:
-                                print("นิ้วกลางซ้าย")
-                            if arr[16, 1] < arr[13, 1]:
-                                print("นิ้วนางซ้าย")
-                            if arr[20, 1] < arr[17, 1]:
-                                print("นิ้วก้อยซ้าย")
-                        if (arr[4, 0] > arr[8, 0]) and (arr[4, 1] < arr[2, 1]):
-                            print("นิ้วโป้งซ้าย")
                 mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
+                if arr[9, 1] < arr[0, 1]:
+                    if (arr[5, 0] >= arr[9, 0]):
+                        if (arr[4, 0] > arr[5, 0]) and (arr[4, 1] < arr[3, 1]):
+                            finger.append("HR")
+                        for i in range(8, 21, 4):
+                            if (arr[i, 1] < arr[i - 2, 1]):
+                                finger.append(f"{int(i/4) - 1}R")
+                    else:
+                        if (arr[4, 0] < arr[5, 0]) and (arr[4, 1] < arr[3, 1]):
+                            finger.append("HL")
+                        for i in range(8, 21, 4):
+                            if (arr[i, 1] < arr[i - 2, 1]):
+                                finger.append(f"{int(i/4) - 1}L")
+                else:
+                    if (arr[5, 0] <= arr[9, 0]):
+                        if (arr[4, 0] > arr[5, 0]) and (arr[4, 1] > arr[3, 1]):
+                            finger.append("HR")
+                        for i in range(8, 21, 4):
+                            if (arr[i, 1] > arr[i - 2, 1]):
+                                finger.append(f"{int(i/4) - 1}R")
+                    else:
+                        if (arr[4, 0] < arr[5, 0]) and (arr[4, 1] > arr[3, 1]):
+                            finger.append("HL")
+                        for i in range(8, 21, 4):
+                            if (arr[i, 1] > arr[i - 2, 1]):
+                                finger.append(f"{int(i/4) - 1}L")
         
+        cv2.putText(frame, "Finger : ", (10, 70), cv2.FONT_HERSHEY_PLAIN, 1, (218, 224, 159), 2)
+        cv2.putText(frame, f"Finger Count: ", (10, 40), cv2.FONT_HERSHEY_PLAIN, 2, (218, 224, 159), 3)
+        if len(finger) != 0:
+            cv2.putText(frame, f"{' '.join(finger)}", (85, 70), cv2.FONT_HERSHEY_PLAIN, 1, (47, 209, 29), 2)
+            cv2.putText(frame, f"{str(len(finger))}", (245, 40), cv2.FONT_HERSHEY_PLAIN, 2, (47, 209, 29), 3)
+        else:
+            cv2.putText(frame, "None", (85, 70), cv2.FONT_HERSHEY_PLAIN, 1, (57, 130, 247), 2)
+            cv2.putText(frame, f"{str(len(finger))}", (245, 40), cv2.FONT_HERSHEY_PLAIN, 2, (57, 130, 247), 3)
         if check == True:
             cv2.imshow("Video", frame)
             if cv2.waitKey(1) & 0xFF == ord('e'):
