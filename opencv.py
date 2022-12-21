fd2e2e824172e6c82fddcdfc3bbbbb3eae900e30
Entity014 <radlove.012@gmail.com>
2022-12-21 18:58:56 +0700

    function v.1.2

--- opencv.py
+++ opencv.py
@@ -1,13 +1,12 @@
 import cv2
 import mediapipe as mp
+import numpy as np
 import time
 
 def videoCap():
     cap = cv2.VideoCapture(0)
     while True:
         check, frame = cap.read()
-        cv2.flip(frame, 1)
-        
         cv2.putText(frame, "Press E to close", (200, 470), cv2.FONT_HERSHEY_PLAIN, 2, (57, 130, 247), 3)
         if check == True:
             cv2.imshow("Video", frame)
@@ -27,7 +26,6 @@ def detectFinger():
     
     while True:
         check, frame = cap.read()
-        cv2.flip(frame, 1)
         imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
         print(results.multi_hand_landmarks)
@@ -65,7 +63,6 @@ def detectPos():
             fps = frame_count / elapsed_time
         
         check, frame = cap.read()
-        cv2.flip(frame, 1)
         imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
         # * print(results.multi_hand_landmarks)
@@ -101,7 +98,6 @@ def detectSym():
     
     while True:
         check, frame = cap.read()
-        cv2.flip(frame, 1)
         imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
         # * print(results.multi_hand_landmarks)
@@ -126,6 +122,84 @@ def detectSym():
         
         cv2.putText(frame, str(int(Nfing)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0 ,255), 3)
         cv2.putText(frame, "Press E to close", (200, 470), cv2.FONT_HERSHEY_PLAIN, 2, (57, 130, 247), 3)
+        
+        if check == True:
+            cv2.imshow("Video", frame)
+            if cv2.waitKey(1) & 0xFF == ord('e'):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+def detectHand():
+    cap = cv2.VideoCapture(0)
+    
+    arr = np.zeros((21, 2))
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
+    while cap.isOpened():
+        check, frame = cap.read()
+        imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    h, w, c = frame.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    arr[id] = [cx, cy]
+                if arr[0, 1] > arr[8, 1]:
+                    if arr[4, 0] > arr[17, 0]:
+                        if arr[0, 0] < arr[5, 0]:
+                            if arr[8, 1] < arr[5, 1]:
+                                print("นิ้วชี้ขวา")
+                            if arr[12, 1] < arr[9, 1]:
+                                print("นิ้วกลางขวา")
+                            if arr[16, 1] < arr[13, 1]:
+                                print("นิ้วนางขวา")
+                            if arr[20, 1] < arr[17, 1]:
+                                print("นิ้วก้อยขวา")
+                        if (arr[4, 0] > arr[8, 0]) and (arr[4, 1] < arr[2, 1]):
+                            print("นิ้วโป้งขวา")
+                    else:
+                        if arr[0, 0] > arr[5, 0]:
+                            if arr[8, 1] < arr[5, 1]:
+                                print("นิ้วชี้ซ้าย")
+                            if arr[12, 1] < arr[9, 1]:
+                                print("นิ้วกลางซ้าย")
+                            if arr[16, 1] < arr[13, 1]:
+                                print("นิ้วนางซ้าย")
+                            if arr[20, 1] < arr[17, 1]:
+                                print("นิ้วก้อยซ้าย")
+                        if (arr[4, 0] < arr[8, 0]) and (arr[4, 1] < arr[2, 1]):
+                            print("นิ้วโป้งซ้าย")
+                elif arr[0, 1] < arr[8, 1]:
+                    if arr[4, 0] < arr[20, 0]:
+                        if arr[0, 0] < arr[5, 0]:
+                            if arr[8, 1] > arr[5, 1]:
+                                print("นิ้วชี้ขวา")
+                            if arr[12, 1] > arr[9, 1]:
+                                print("นิ้วกลางขวา")
+                            if arr[16, 1] > arr[13, 1]:
+                                print("นิ้วนางขวา")
+                            if arr[20, 1] > arr[17, 1]:
+                                print("นิ้วก้อยขวา")
+                        if (arr[4, 0] > arr[8, 0]) and (arr[4, 1] > arr[2, 1]):
+                            print("นิ้วโป้งขวา")
+                    else:
+                        if arr[0, 0] > arr[5, 0]:
+                            if arr[8, 1] < arr[5, 1]:
+                                print("นิ้วชี้ซ้าย")
+                            if arr[12, 1] < arr[9, 1]:
+                                print("นิ้วกลางซ้าย")
+                            if arr[16, 1] < arr[13, 1]:
+                                print("นิ้วนางซ้าย")
+                            if arr[20, 1] < arr[17, 1]:
+                                print("นิ้วก้อยซ้าย")
+                        if (arr[4, 0] > arr[8, 0]) and (arr[4, 1] < arr[2, 1]):
+                            print("นิ้วโป้งซ้าย")
+                mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
         
         if check == True:
             cv2.imshow("Video", frame)
